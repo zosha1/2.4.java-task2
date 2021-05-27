113931ef4319f5fcb706d61a1dbab0a8d7b4353c
Zosha <posyamyka@gmail.com>
2021-05-27 17:17:48 +0300

    commit2

--- src/test/java/ru/netology/bonus/BonusServiceTest.java
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -2,7 +2,6 @@ package ru.netology.bonus;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
